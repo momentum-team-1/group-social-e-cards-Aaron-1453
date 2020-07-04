a422de430d42db84c113897c0635b372c5b74217
AaronOBrien1453 <aatobrien@gmail.com>
2020-07-04 03:00:09 -0400

    Render card image, content, author

--- social-cards/src/components/Profile.js
+++ social-cards/src/components/Profile.js
@@ -21,9 +21,31 @@ const Profile = (props) => {
   return (
     <div>
       <Nav username={username} setUsername={props.setUsername} />
-      <p class='title'>Hello, {username}  </p>
-      {cards &&
-        cards.map((card, idx) => <p key={idx}>{card.author.id}</p>)}
+      <p className='title'>Hello, {username}  </p>
+      {cards && (
+        <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+          {cards.map((card) => {
+            return (
+              <div key={card.id} style={{ width: 350 }}>
+                <div className='card'>
+                  <div className='card-image'>
+                    <figure className='image is-4by3'>
+                      <img src='https://bulma.io/images/placeholders/1280x960.png' alt={`${card.id}-placeholder-img`} />
+                    </figure>
+                  </div>
+                  <div className='card-content'>
+                    <div className='content'>
+                      <p>{card.message}</p>
+                      <p>From: {card.author.username}</p>
+                      <p>{card.posted_at}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      )}
     </div>
   )
 }
